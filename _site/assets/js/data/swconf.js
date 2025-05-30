2339b535de774a01d2bde0bdf4884c4955db1f47
dreysanox <dreysanox@gmail.com>
2025-05-30 16:37:47 +0200

    retomamos

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748614703',resources: [
+    cacheName: 'chirpy-1748614885',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
