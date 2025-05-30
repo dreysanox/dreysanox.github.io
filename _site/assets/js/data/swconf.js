b4853152dd90533b15d3e73f4b08e02b91322fc7
dreysanox <dreysanox@gmail.com>
2025-05-30 18:05:23 +0200

    Por fin entiendo jekyll

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1748620803',resources: [
+    cacheName: 'chirpy-1748620951',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
